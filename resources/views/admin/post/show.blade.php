46116736da2df7ef0381c393eff6b79dcfde2895
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-28 18:53:15 +0100

    added type name

--- resources/views/admin/post/show.blade.php
+++ resources/views/admin/post/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                {{ $post->author }}
+                {{ $post->author }} | || | {{ $post->type->name }}
             </div>
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }}</h5>
